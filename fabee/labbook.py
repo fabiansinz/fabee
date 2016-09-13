40b69c05e1fa471353f92ccf844768d138b25b20
Fabian Sinz <sinz@bcm.edu>
2016-09-13 11:56:53 -0500

    update assessment

--- fabee/labbook.py
+++ fabee/labbook.py
@@ -1,9 +1,13 @@
+import os
+from glob import glob
+
 from pprint import pprint
 
 import datajoint as dj
 from commons import inj
 from djaddon import hdf5
 from commons import mice, virus, inj
+
 from scipy.misc import imread
 schema = dj.schema('fabee_labbook', locals())
 
@@ -16,15 +20,41 @@ class InjectionAssessment(dj.Manual):
     ---
     success     : tinyint       # whether injection was succeful or not
     notes       : varchar(1024) # evaluation
-    image=null  : longblob      # image of the injection site
     """
 
     def populate(self):
+        path = input('Please enter image path (format PATH/{animal_id}/*.jpg): ')
+        path = os.path.expanduser(path)
+        if path[-1] != '/':
+            path += '/'
+        path += '{animal_id}/*.jpg'
         todo = inj.VirusInjection() * mice.Death() * virus.Virus() - self
-        for k in todo.fetch.as_dict:
-            pprint(k)
-            k['notes'] = input('Please input notes: ')
-            path = input('Please input path to image (leave empty for no image)')
-            if path.strip():
-                k['image'] = scipy.imread(path).astype(float)
-            self.insert1(k, ignore_extra_fields=True)
+        with self.connection.transaction:
+            for k in todo.fetch.as_dict:
+                pprint(k)
+                k['notes'] = input('Please input notes: ')
+                k['success'] = int(input('Target region successfully infected? [1,0]: '))
+                self.insert1(k, ignore_extra_fields=True)
+
+                images = glob(path.format(**k))
+                if len(images) > 0:
+                    imp = input('Found images in ~/Dropbox/experiments/{animal_id}/. Import? [Y/n]'.format(**k))
+                    if imp.strip().lower() in ('', 'y'):
+                        for i, image in enumerate(images):
+                            print('Loading ', image)
+                            k['image'] = imread(image).astype(float)
+                            k['image_id'] = i
+                            self.Image().insert1(k, ignore_extra_fields=True)
+                if input('Exit? [y/N]').strip().lower() == 'y':
+                    break
+
+
+    class Image(dj.Part):
+        definition = """
+        # image of the injection
+
+        -> InjectionAssessment
+        image_id    : smallint
+        ---
+        image       : longblob
+        """
